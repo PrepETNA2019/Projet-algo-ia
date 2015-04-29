e403355b43e4be060a269aa9d2dad9a2990bea32
Nolan VIEIRA <vieira.nolan@gmail.com>
2015-04-29 16:43:14 +0200

    add ia class + map test

---
+++ ia.php
@@ -0,0 +1,52 @@
+<?php
+
+class Ia{
+    var $energie_start;
+    var $energie_drop;
+    var $distance;
+    var $ia;
+    var $sortie;
+    var $carte;
+    
+    function __construct($argc, $argv){
+        if ($argc != 4){
+            echo "vous n'avez pas respecté les parametres\n";
+            return false;
+        }
+        var_dump($argv);
+        if (!is_numeric($argv[1])){
+            echo "le 1er argument n'est pas un nombre: fin du programme\n";
+            return false;
+        }
+        $this->energie_start = intval($argv[1]);
+        if (!is_numeric($argv[2])){
+            echo "le 2eme argument n'est pas un nombre: fin du programme\n";
+            return false;
+        }
+        $this->energie_drop = intval($argv[2]);
+        if (!is_readable($argv[3])){
+            echo "le 3eme arguments n'est pas un fichier ou n'est pas en lecture: fin du programme\n";
+            return false;
+        }
+        Ia::create_map_test();
+       
+    }
+    
+    function create_map_test(){
+        $map[0] = array(' ','@',' ','+',' ',' ','+',' ',' ', ' ');
+        $map[1] = array(' ','+',' ',' ','+',' ',' ',' ',' ', ' ');
+        $map[2] = array(' ',' ',' ',' ',' ',' ',' ',' ',' ', ' ');
+        $map[3] = array(' ','+',' ','+','+',' ',' ',' ','+', ' ');
+        $map[4] = array(' ',' ',' ',' ',' ',' ',' ',' ',' ', ' ');
+        $map[5] = array(' ','+',' ',' ','+',' ',' ',' ',' ', ' ');
+        $map[6] = array(' ',' ',' ',' ',' ',' ',' ',' ',' ', ' ');
+        $map[7] = array(' ',' ',' ',' ',' ',' ','+',' ','#', ' ');
+        $map[8] = array(' ',' ',' ',' ',' ',' ',' ',' ',' ', ' ');
+        $map[9] = array(' ','+',' ',' ','+',' ',' ',' ',' ', ' ');
+        $this->carte = $map;
+    }
+}
+
+$ia_test = new Ia($argc, $argv);
+var_dump($ia_test);
+?>
